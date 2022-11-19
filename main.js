0dba817eff0b9d0ad901daf4d6c7dd2127a7fe27
bhaybhiisparks <bhaybhiisparks@gmail.com>
2022-11-19 12:27:41 +0100

    fixed previous button

--- main.js
+++ main.js
@@ -1,25 +1,67 @@
 const buttons = document.querySelectorAll("[data-buttons-button]");
-const imagesLi = document.querySelectorAll(".slide");
-console.log(buttons)
+const nextBtn = document.querySelector("[data-next]");
+const prevBtn = document.querySelector("[data-prev]");
+const imagesLi = document.querySelectorAll("li img");
+// console.log(buttons)
+
+prevBtn.addEventListener("click", prev);
+nextBtn.addEventListener("click", next);
 
 
+function next(){
+    const slides = document.querySelector("[data-slides]");
+    const activeSlide = slides.querySelector("[data-active]");
+    let newIndex = [...slides.children].indexOf(activeSlide) + 1;
+    if (newIndex < 0) {
+        newIndex = slides.children.length - 1;
+    }
+    if (newIndex >= slides.children.length) {
+        newIndex = 0;
+    } 
+    slides.children[newIndex].dataset.active = true;
+    delete activeSlide.dataset.active;
+}
+
+function prev(){
+    const slides = document.querySelector("[data-slides]");
+    const activeSlide = slides.querySelector("[data-active]");
+    let newIndex = [...slides.children].indexOf(activeSlide) - 1;
+    if (newIndex < 0) {
+        newIndex = slides.children.length - 1;
+    }
+    if (newIndex >= slides.children.length) {
+        newIndex = 0;
+    } 
+    slides.children[newIndex].dataset.active = true;
+    delete activeSlide.dataset.active;
+}
+
+const container = document.querySelector("[data-container]");
+const mainbutton = document.querySelector("[data-button-container]");
 
 
-buttons.forEach(button =>{
-    button.addEventListener("click", ()=>{
-        const offset = button.dataset.buttonButton === "next" ? 1 : -1;
-        const slides = button.closest("[data-container]").querySelector("[data-slides");
+imagesLi.forEach(image =>{
+    image.addEventListener("mouseover", (e) =>{
+        buttons.forEach(button =>{
+            button.classList.remove("default");
+        })
+    })
 
-        const activeSlide = slides.querySelector("[data-active]");
-        let newIndex = [...slides.children].indexOf(activeSlide) + offset;
-        if (newIndex < 0){
-            newIndex = slides.children.length - 1;
-        }
-        if (newIndex >= slides.children.length) {
-            newIndex = 0 ;
-        } 
-
-        slides.children[newIndex].dataset.active = true;
-        delete activeSlide.dataset.active;
+    image.addEventListener("mouseout", () =>{
+        buttons.forEach(button =>{
+            button.classList.add("default");
+        })
     })
 })
+
+
+buttons.addEventListener("mouseover", (e) =>{
+    buttons.forEach(button =>{
+        button.classList.remove("default");
+    })
+})
+
+
+
+
+
